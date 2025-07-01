1f7b55cfc4ef14b449a125d50e0ccdb76aa8d890
UniT-17 <mintayzamt17@gmail.com>
2025-07-01 08:18:02 +0700

    Done - Nav Header for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,15 +33,17 @@ function Header() {
         </ul>
 
         {/* For Mobile */}
-        <div className="fixed inset-0 z-50 bg-sky-400 opacity-75 ">
-          <div className="">
-            <Link to={"/shop"} className="text-3xl">
+        <div
+          className={`fixed inset-0 z-50 bg-sky-400 opacity-75 transform ${isOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-100 ease-in-out `}
+        >
+          <div className="flex flex-col items-center h-full justify-center gap-8">
+            <Link to={"/shop"} className="text-3xl" onClick={toogleOpen}>
               Shop
             </Link>
-            <Link to={"/"} className="text-3xl">
+            <Link to={"/"} className="text-3xl" onClick={toogleOpen}>
               Home
             </Link>
-            <Link to={"cart"} className="text-3xl">
+            <Link to={"cart"} className="text-3xl" onClick={toogleOpen}>
               Cart
             </Link>
           </div>
