e96771606243ac8a636fa889e1651ebcf3ac88ec
UniT-17 <mintayzamt17@gmail.com>
2025-07-04 23:33:46 +0700

    Done - Not Found Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
 export default function NotFound() {
-  return <div>NotFount</div>;
+  return (
+    <section className="flex grow">
+      <div className="mx-auto container my-auto text-center">
+        <h1 className="text-3xl font-bold md:text-4xl">404 Not Found!</h1>
+        <p className="my-2 md:text-xl">
+          Opps!The page you're looking for doesn't exits!
+        </p>
+        <Link to="/">
+          <Button className="bg-sky-500 hover:bg-sky-600">Go Back Home</Button>
+        </Link>
+      </div>
+    </section>
+  );
 }
